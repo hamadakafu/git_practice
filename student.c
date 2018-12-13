d8a43a79db88b7371247e2b50df77ca29a71f2f7
hamadakafu <kafu.h1998@gmail.com>
2018-12-13 10:37:51 +0900

    add function in student
    expand student

--- student.c
+++ student.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "student.h"
 
 void print_student(Student s) {
     printf("%d: %s\n", s.id, s.name);
+}
+
+Student *new_student() {
+    Student *s = malloc(sizeof(Student));
+    if (s == NULL) {
+        fprintf(stderr, "error: can't malloc Student in new_student.\n");
+        exit(1);
+    }
+    s->id = 0;
+    strcpy(s->name, "unknown");
+    return s;
+}
+
+int set_params(Student *s, int id, char *name) {
+    if (sizeof(*name) >= 256) {
+        fprintf(stderr, "error: name is too long in set_params.\n");
+        return 1;
+    } else {
+        s->id = id;
+        strcpy(s->name, name);
+        return 0;
+    }
 }
--- student.h
+++ student.h
@@ -7,5 +7,7 @@ typedef struct Student_tag {
 } Student;
 
 void print_student(Student s);
+Student *new_student();
+int set_param_student(Student *s, int id, char *name);
 
 #endif
